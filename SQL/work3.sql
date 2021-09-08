e75958efc3d9938838a4b527185b04c493a890c1
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-09-08 15:46:48 +0300

    work3

---
+++ SQL/work3.sql
@@ -0,0 +1,5 @@
+SELECT country FROM country WHERE country LIKE 'A%a';
+
+SELECT country FROM country WHERE country LIKE '_____%n';
+
+SELECT * FROM film WHERE title LIKE 'C%' AND (length>90 AND rental_rate=2.99);
