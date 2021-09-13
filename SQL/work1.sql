4c899e6a2a7b5bc72cef15510b2118360534e1ab
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-09-13 13:38:33 +0300

    work1

--- SQL/work1.sql
+++ SQL/work1.sql
@@ -7,4 +7,3 @@ SELECT * FROM film WHERE  rental_rate=0.99 AND (replacement_cost=12.99 OR replac
 SELECT last_name FROM customer WHERE first_name='Mary';
 
 SELECT * FROM film WHERE NOT length>50 AND NOT (rental_rate=2.99 OR rental_rate=4.99);
-
